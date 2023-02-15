963782e12cc99682337e6a143751d42186f6d434
Pierre <pierrepitoiset77@gmail.com>
2023-02-15 17:15:38 +0100

    last small fixes

--- models/movies.js
+++ models/movies.js
@@ -1,10 +1,8 @@
-var mongoose = require('mongoose')
+var mongoose = require('mongoose');
 
 var movieSchema = mongoose.Schema({
-    movieName: String,
-    movieImg: String
-})
+	movieName: String,
+	movieImg: String,
+});
 
-var movieModel = mongoose.model('movies', movieSchema)
-
-module.exports = movieModel;
+module.exports = mongoose.model('movies', movieSchema);
